7a78d85b8a39296d64caa4802493bcee2aea00a1
Pablo Rial Sartirana <65549425+rialpablojhotmailcom@users.noreply.github.com>
2024-05-31 22:18:51 +0100

    add folder structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+import Dashboard from "./containers/dashboard/Dashboard";
+
+function App() {
+  return (
+    <div className="app">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
