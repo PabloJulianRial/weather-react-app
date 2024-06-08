0895db17d06de73a2347fc62ad4cf58b1a53879d
Pablo Rial Sartirana <65549425+rialpablojhotmailcom@users.noreply.github.com>
2024-06-08 11:47:23 +0100

    add weather icon dinamically and location background

--- src/components/weatherImage/WeatherImage.tsx
+++ src/components/weatherImage/WeatherImage.tsx
@@ -1,74 +1,73 @@
 import { useWeather } from "../../context/WeatherContext";
 import "./WeatherImage.scss";
 
-const { currentWeather, forecast } = useWeather();
-if (!currentWeather || !forecast) {
-  console.log("Image not found");
-}
-
-let imageSrc = "";
-
-switch (currentWeather?.current.condition.text) {
-  case "Partly cloudy":
-    imageSrc = "src/assets/some-clouds.png";
-    break;
-  case "Clear":
-    imageSrc = "src/assets/sun.png";
-    break;
-  case "Overcast":
-    imageSrc = "March";
-    break;
-  case "Cloudy":
-    imageSrc = "April";
-    break;
-  case "Light rain":
-    imageSrc = "May";
-    break;
-  case "Moderate rain":
-    imageSrc = "June";
-    break;
-  case "Heavy rain":
-    imageSrc = "July";
-    break;
-  case "Light snow":
-    imageSrc = "August";
-    break;
-  case "Moderate snow":
-    imageSrc = "August";
-    break;
-  case "Heavy snow":
-    imageSrc = "August";
-    break;
-  case "Thunderstorms":
-    imageSrc = "August";
-    break;
-  case "Foggy":
-    imageSrc = "August";
-    break;
-  case "Mist":
-    imageSrc = "August";
-    break;
-  case "Haze":
-    imageSrc = "August";
-    break;
-  case "Smoke":
-    imageSrc = "August";
-    break;
-  case "Drizzle":
-    imageSrc = "August";
-    break;
-  case "Freezing rain":
-    imageSrc = "August";
-    break;
-  case "Sleet":
-    imageSrc = "August";
-    break;
-  default:
-    throw new Error("Invalid month number extracted from the string.");
-}
-
 const WeatherImage = () => {
-  return <img src="imageSrc" className="weather-image"></img>;
+  const { currentWeather, forecast } = useWeather();
+  if (!currentWeather || !forecast) {
+    console.log("Image not found");
+  }
+
+  let imageSrc = "";
+
+  switch (currentWeather?.current.condition.text) {
+    case "Partly cloudy":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Clear":
+      imageSrc = "src/assets/sun.png";
+      break;
+    case "Overcast":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Cloudy":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Light rain":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Moderate rain":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Heavy rain":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Light snow":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Moderate snow":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Heavy snow":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Thunderstorms":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Foggy":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Mist":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Haze":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Smoke":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Drizzle":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Freezing rain":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    case "Sleet":
+      imageSrc = "src/assets/some-clouds.png";
+      break;
+    default:
+      throw new Error("Invalid month number extracted from the string.");
+  }
+  return <img src={imageSrc} className="weather-image" />;
 };
 
 export default WeatherImage;
